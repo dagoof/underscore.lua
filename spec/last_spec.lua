d13563925d6f6e11cc20228cabfd46ad9cd7a381
Peter <dagoof@gmail.com>
2011-03-18 19:07:42 +1100

    ordering changed in last_spec

--- spec/last_spec.lua
+++ spec/last_spec.lua
@@ -1,15 +1,16 @@
 require 'spec_helper'
 
 describe['_.last'] = function()
-    it['should return the last item'] = function()
+    before = function()
         input = {1, 2, 3, 4}
+    end
+    it['should return the last item'] = function()
         result = _.last(input)
         expect(result).should_be(4)
     end
 
     describe['when passing in a size'] = function()
         it['should return an array with the last n items'] = function()
-            input = {1, 2, 3, 4}
             result = _.last(input, 2)
             expect(result).should_equal {3, 4}
         end
